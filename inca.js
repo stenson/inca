f3b8067a1f49efed87172c620edf431a67628983
Rob Stenson <rob.stenson@gmail.com>
2011-10-04 08:39:25 -0700

    bump package, cleanup command implementation

--- inca.js
+++ inca.js
@@ -9,18 +9,21 @@ var fs = require("fs");
 // constants
 var dirs = ["json", "mustaches", "public"];
 
-function welcome(req, res) {
-  res.render("index");
-}
+var pages = {
+  welcome: function(req, res) {
+    req.params.page = "index";
+    pages.dynamic(req, res);
+  },
 
-function cullMustaches(req, res) {
-  var jsons = {};
-  fs.readdirSync("json").forEach(function(file) {
-    jsons[file.split(".")[0]] = JSON.parse(fs.readFileSync("json/"+file, "utf-8"));
-  });
-
-  res.render(req.params.page, { locals: jsons });
-}
+  dynamic: function(req, res) {
+    var jsons = {};
+    fs.readdirSync("json").forEach(function(file) {
+      jsons[file.split(".")[0]] = JSON.parse(fs.readFileSync("json/"+file, "utf-8"));
+    });
+    console.log("rendering "+req.params.page);
+    res.render(req.params.page, { locals: jsons });
+  }
+};
 
 var commands = {
   // the web-server command (i.e. no command at all)
@@ -35,8 +38,8 @@ var commands = {
     app.use(express.static("public"));
 
     // look
-    app.get("/", welcome);
-    app.get("/:page", cullMustaches);
+    app.get("/", pages.welcome);
+    app.get("/:page", pages.dynamic);
 
     // & listen
     app.listen(port);
