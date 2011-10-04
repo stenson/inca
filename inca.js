25d6a97dc33415137fd8940db06a2f6c1ec2085d
Rob Stenson <rob.stenson@gmail.com>
2011-10-04 08:14:55 -0700

    adding more actions to the general inca tool

--- inca.js
+++ inca.js
@@ -1,12 +1,19 @@
+// npm dependencies
 var express = require("express");
 var colors = require("colors");
+
+// node stdlibs
+var exec = require("child_process").exec;
 var fs = require("fs");
+
+// constants
+var dirs = ["json", "mustaches", "public"];
 
 function welcome(req, res) {
   res.render("index");
 }
 
-function mustachio(req, res) {
+function cullMustaches(req, res) {
   var jsons = {};
   fs.readdirSync("json").forEach(function(file) {
     jsons[file.split(".")[0]] = JSON.parse(fs.readFileSync("json/"+file, "utf-8"));
@@ -15,31 +22,51 @@ function mustachio(req, res) {
   res.render(req.params.page, { locals: jsons });
 }
 
-function run() {
-  var app = express.createServer();
-  var port = 8083;
+var commands = {
+  // the web-server command (i.e. no command at all)
+  run: function() {
+    var app = express.createServer();
+    var port = 8083;
 
-  // stop
-  app.set("view engine", "mustache");
-  app.set("views", "mustaches");
-  app.register(".mustache", require("./tern"));
-  app.use(express.static("public"));
+    // stop
+    app.set("view engine", "mustache");
+    app.set("views", "mustaches");
+    app.register(".mustache", require("./tern"));
+    app.use(express.static("public"));
 
-  // look
-  app.get("/", welcome);
-  app.get("/:page", mustachio);
+    // look
+    app.get("/", welcome);
+    app.get("/:page", cullMustaches);
 
-  // & listen
-  app.listen(port);
+    // & listen
+    app.listen(port);
 
-  // report!
-  console.log("~~~ Welcome to Inca! ~~~".green);
-  console.log("~~~ The server is currently running at localhost:8083 ~~~".blue);
-  console.log("~~~ Ctrl-C to stop the server ~~~".red);
-}
+    // report!
+    console.log("~~~ Welcome to Inca! ~~~".green);
+    console.log("~~~ The server is currently running at localhost:8083 ~~~".blue);
+    console.log("~~~ Ctrl-C to stop the server ~~~".red);
+  },
+
+  init: function() {
+    console.log("\n\n\twriting directories...\n\n".red);
+    dirs.map(function(dir) {
+      exec("mkdir "+dir);
+    });
+  },
+
+  cleanup: function() {
+    console.log("\n\n\tremoving directories...\n\n".red);
+    dirs.map(function(dir) {
+      exec("rmdir "+dir);
+    });
+  }
+};
 
 module.exports = {
-  exec: function(cmd) {
-    run();
+  exec: function(args) {
+    var cmd = args[2];
+    commands[cmd]
+      ? commands[cmd]()
+      : commands.run();
   }
 };
